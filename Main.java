aa7b5aca367d5cddfc6dfda9a319c3fc09c96086
Izelrod05 <irodriguezdiaz@ucmerced.edu>
2025-07-17 22:12:36 +0000

    maid main and junit class

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*Izel Rodriguez Diaz
+07/17/2025
+recursion and junit assigment
+*/
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+  
+  
+  
+  
+    }     
+}
